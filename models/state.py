a6038741a5f03ac0f75327739cce33a5c327903d
habyhaj <habyhaj07@gmail.com>
2023-09-27 13:19:38 -0700

    fixed

--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""Defines the State class."""
 from models.base_model import BaseModel
 
-classtate(BaseModel):
-    """Represents a state.
 
+class State(BaseModel):
+    """Represent a state.
     Attributes:
         name (str): The name of the state.
     """
-    
-        name = ""
 
+    name = ""
